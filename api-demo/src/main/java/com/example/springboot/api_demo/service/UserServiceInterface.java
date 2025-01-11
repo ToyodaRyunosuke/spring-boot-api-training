ef9944613f20d7516f0321dfab57b61611b7a1f9
ToyodaRyunosuke <130950989+ToyodaRyunosuke@users.noreply.github.com>
2025-01-11 15:40:15 +0900

    ユーザー取得登録api追加 (#1)
    Co-authored-by: ryunosuke0214 <ryunosuke0214@au.com>

--- api-demo/src/main/java/com/example/springboot/api_demo/service/UserServiceInterface.java
+++ api-demo/src/main/java/com/example/springboot/api_demo/service/UserServiceInterface.java
@@ -1,9 +1,10 @@
 package com.example.springboot.api_demo.service;
-
 import java.util.List;
-
-import com.example.springboot.api_demo.dto.UserDto;
+import com.example.springboot.api_demo.dto.UserRequestDto;
+import com.example.springboot.api_demo.dto.UserResponseDto;
 
 public interface UserServiceInterface {
-    List<UserDto> findUserList();
+    List<UserResponseDto> findUserList();
+    UserResponseDto findUserById(Long id);
+    UserResponseDto addUser(UserRequestDto dto);
 }
